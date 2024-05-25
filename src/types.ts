a10ee9caf009219856a4d0e02801678fff2d484e
wnopraf <rafael.prieto@chollometro.com>
2024-05-25 22:53:30 +0200

    add types

---
+++ src/types.ts
@@ -0,0 +1,78 @@
+export interface PodCast {
+  feed: {
+    author: {
+      name: {
+        label: string;
+      };
+      uri: {
+        label: string;
+      };
+    };
+  };
+  entry: {
+    "im:name": {
+      label: string;
+    };
+    "im:image": {
+      label: string;
+      attributes: {
+        height: number;
+      };
+    }[];
+    summary: {
+      label: string;
+    };
+    "im:price": {
+      label: string;
+      attributes: {
+        amount: string;
+        currency: "USD";
+      };
+    };
+    "im:contentType": {
+      attributes: {
+        term: "Podcast";
+        label: "Podcast";
+      };
+    };
+    rights: {
+      label: string;
+    };
+    title: {
+      label: string;
+    };
+    link: {
+      attributes: {
+        rel: "alternative";
+        type: "text/html";
+        href: string;
+      };
+    };
+    id: {
+      label: string;
+      attributes: {
+        "im:id": string;
+      };
+    };
+    "im:artist": {
+      label: string;
+      attributes: {
+        href: string;
+      };
+    };
+    category: {
+      attributes: {
+        "im:id": string;
+        term: "Music";
+        scheme: string;
+        label: "Music";
+      };
+    };
+    "im:releaseDate": {
+      label: string;
+      attributes: {
+        label: string;
+      };
+    };
+  }[];
+}
