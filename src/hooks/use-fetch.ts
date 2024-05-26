4ae3247d66cbd7063f2a9488960213e23da05042
wnopraf <rafael.prieto@chollometro.com>
2024-05-26 11:28:48 +0200

    adapt cache logic

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,37 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 let cache: unknown = null;
 let cacheTimeStamp = 0;
+interface CacheResponse<T> {
+ isLoading: boolean;
+ data: T | null;
+ error: null | typeof Error;
+}
 export async function useFetch<CacheData>(
-  url: string,
-  deltaCacheReval: number = 24
+ url: string,
+ deltaCacheReval: number = 24
 ) {
-  const [isLoading, setIsloading] = useState(false);
+ const [apiResult, setApiResult] = useState<CacheResponse<CacheData>>({
+  isLoading: false,
+  data: null,
+  error: null,
+ });
 
-  const deltaCacheRevalToMs = deltaCacheReval * 60 * 60 * 1000;
-  const deltaCacheDiff = Date.now() - cacheTimeStamp;
-  const isRevalidated = deltaCacheDiff > deltaCacheRevalToMs;
-  if (isRevalidated) {
+ useEffect(() => {
+  fetchApi();
+  async function fetchApi() {
+   const deltaCacheRevalToMs = deltaCacheReval * 60 * 60 * 1000;
+   const deltaCacheDiff = Date.now() - cacheTimeStamp;
+   const isRevalidated = deltaCacheDiff > deltaCacheRevalToMs;
+   setApiResult((state) => ({ ...state, isLoading: true }));
+   if (isRevalidated) {
     try {
-      setIsloading(true);
-      const data = await fetch(url).then((data) => data.json());
-      setIsloading(false);
-      cache = data;
-      cacheTimeStamp = Date.now();
-      return {
-        data,
-        isLoading,
-      };
-    } catch (error) {
-      return {
-        error,
-      };
+     const data = (await fetch(url).then((data) => data.json())) as CacheData;
+     setApiResult((state) => ({ ...state, data, isLoading: false }));
+     cache = data;
+     cacheTimeStamp = Date.now();
+    } catch (e) {
+     const error = e as typeof Error;
+     setApiResult((state) => ({ ...state, isLoading: false, error }));
     }
-  } else {
-    return {
-      data: cache as CacheData,
-      isLoading,
-      error: null,
-    };
+   } else {
+    setApiResult((state) => ({ ...state, data: cache as CacheData }));
+   }
   }
+ }, []);
+ return apiResult;
 }
