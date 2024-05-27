ea79db6f067c0d38ae8c6a540bea0b24e3113645
wnopraf <rafael.prieto@chollometro.com>
2024-05-27 18:04:01 +0200

    create a singleton cache storage interface

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,13 +1,33 @@
-import { Dispatch, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
-let cache: unknown = null;
+interface CacheStore {
+ store: Cache;
+ getCache(key: string): Cache["key"];
+ setCache(key: string, cache: any): void;
+}
+type Cache = {
+ [key: string]: any;
+};
+const cacheStore: CacheStore = {
+ store: {},
+ getCache(key: string) {
+  return this.store[key];
+ },
+ setCache<T>(key: string, cache: T) {
+  this.store[key] = cache;
+ },
+};
 let cacheTimeStamp = 0;
 interface CacheResponse<T> {
  isLoading: boolean;
  data: T | null;
  error: null | typeof Error;
 }
-export function useFetch<CacheData>(url: string, deltaCacheReval: number = 24) {
+export function useFetch<CacheData>(
+ id: string,
+ url: string,
+ deltaCacheReval: number = 24
+) {
  const [apiResult, setApiResult] = useState<CacheResponse<CacheData>>({
   isLoading: true,
   data: null,
@@ -26,13 +46,14 @@ export function useFetch<CacheData>(url: string, deltaCacheReval: number = 24) {
      const data = (await fetch(url).then((data) => data.json())) as CacheData;
      setApiResult((state) => ({ ...state, data, isLoading: false }));
 
-     cache = data;
+     cacheStore.setCache(id, data);
      cacheTimeStamp = Date.now();
     } catch (e) {
      const error = e as typeof Error;
      setApiResult((state) => ({ ...state, isLoading: false, error }));
     }
    } else {
+    const cache = cacheStore.getCache(id);
     setApiResult((state) => ({ ...state, data: cache as CacheData }));
    }
   }
