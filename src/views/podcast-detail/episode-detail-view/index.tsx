16dfc26ad2a7b51cf28bcba5e03acf3d83dd2ba4
wnopraf <rafael.prieto@chollometro.com>
2024-05-29 14:58:04 +0200

    add uppercse flter on search  component

--- src/views/podcast-detail/episode-detail-view/index.tsx
+++ src/views/podcast-detail/episode-detail-view/index.tsx
@@ -9,7 +9,7 @@ export function EpisodeDetail() {
   });
 
   return (
-    <div>
+    <div className=" p-5 shadow shadow-gray-500">
       <div>
         <h1 className="text-2xl font-semibold mb-5">{episode?.trackName}</h1>
         <p dangerouslySetInnerHTML={{ __html: episode?.description }}></p>
--- src/views/podcast-list/hooks/search-util.tsx
+++ src/views/podcast-list/hooks/search-util.tsx
@@ -2,22 +2,22 @@ import { useState } from "react";
 import { PodCast } from "../../../types";
 
 export function useSearch(podcasts: PodCast[] | null) {
- const [search, setSearch] = useState<string>();
+  const [search, setSearch] = useState<string>();
 
- const filteredPodcasts = podcasts
-  ?.map((elm) => {
-   elm["im:name"].label = elm["im:name"].label.toLowerCase();
-   elm["im:artist"].label = elm["im:artist"].label.toLowerCase();
-   return elm;
-  })
-  .filter((elm) => {
-   return (
-    elm["im:name"].label.includes(search as string) ||
-    elm["im:artist"].label.includes(search as string)
-   );
-  });
- return {
-  setSearch,
-  filteredPodcasts,
- };
+  const filteredPodcasts = podcasts
+    ?.map((elm) => {
+      elm["im:name"].label = elm["im:name"].label.toLowerCase();
+      elm["im:artist"].label = elm["im:artist"].label.toLowerCase();
+      return elm;
+    })
+    .filter((elm) => {
+      return (
+        elm["im:name"].label.includes(search?.toLocaleLowerCase() as string) ||
+        elm["im:artist"].label.includes(search?.toLocaleLowerCase() as string)
+      );
+    });
+  return {
+    setSearch,
+    filteredPodcasts,
+  };
 }
