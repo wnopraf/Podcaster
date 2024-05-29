f8ca5dfc7a9393a5730cd31f0f8a2f6927873297
wnopraf <rafael.prieto@chollometro.com>
2024-05-29 10:12:09 +0200

    improve table shadow

--- src/views/podcast-detail/components/episode-list.tsx
+++ src/views/podcast-detail/components/episode-list.tsx
@@ -20,39 +20,41 @@ export function EpisodeList() {
   const data = useLoaderData() as IPodcastDetail;
   const href = useHref("/");
   return (
-    <div>
+    <>
       <header className="shadow shadow-gray-400">
         <h1 className=" p-2 text-2xl font-semibold">
           Episodes: &nbsp;{data.resultCount}
         </h1>
       </header>
-      <table className=" w-full mt-10 p-1 shadow shadow-gray-400 ">
-        <thead className="grid grid-cols-[70%,1fr,1fr] pt-8 pb-2">
-          <th className=" pl-3 text-left">Title</th>
-          <th className="  text-left">Date</th>
-          <th className="  text-left">Duration</th>
-        </thead>
-        {data.results.map((elm) => {
-          return (
-            <tr className=" grid grid-cols-[70%,1fr,1fr] items-center h-10 even:bg-gray-100 border-t-2 border-b-2 border-gray-300">
-              <td className="text-blue-500 pl-3">
-                {
-                  <Link to={href + "/episode/" + elm.trackId}>
-                    {elm.trackName}
-                  </Link>
-                }
-              </td>
-              <td className="text-gray-600">
-                {new Date(elm.releaseDate).toLocaleDateString()}
-              </td>
-              <td className="text-gray-600">
-                {millsToMinuteFormat(elm.trackTimeMillis)}
-              </td>
-            </tr>
-          );
-        })}
-      </table>
-    </div>
+      <div className="px-4 shadow shadow-gray-500">
+        <table className=" w-full mt-10 p-1  ">
+          <thead className="grid grid-cols-[70%,1fr,1fr] pt-8 pb-2">
+            <th className=" pl-3 text-left">Title</th>
+            <th className="  text-left">Date</th>
+            <th className="  text-left">Duration</th>
+          </thead>
+          {data.results.map((elm) => {
+            return (
+              <tr className=" grid grid-cols-[70%,1fr,1fr] items-center h-10 even:bg-gray-100 border-t-2 border-b-2 border-gray-200">
+                <td className="text-blue-500 pl-3">
+                  {
+                    <Link to={href + "/episode/" + elm.trackId}>
+                      {elm.trackName}
+                    </Link>
+                  }
+                </td>
+                <td className="text-gray-600">
+                  {new Date(elm.releaseDate).toLocaleDateString()}
+                </td>
+                <td className="text-gray-600">
+                  {millsToMinuteFormat(elm.trackTimeMillis)}
+                </td>
+              </tr>
+            );
+          })}
+        </table>
+      </div>
+    </>
   );
 }
 
