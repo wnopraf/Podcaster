a29dc7ef5353bd9af30d3a001212dbff191e4ac8
wnopraf <rafael.prieto@chollometro.com>
2024-05-29 07:35:53 +0200

    add IpodcastDetail & fix tipo

--- src/types.ts
+++ src/types.ts
@@ -1,79 +1,96 @@
-export type PodCast = PodCastApi["entry"][0];
+export type PodCast = PodCastApi["feed"]["entry"][0];
 export interface PodCastApi {
- feed: {
-  author: {
-   name: {
-    label: string;
-   };
-   uri: {
-    label: string;
-   };
+  feed: {
+    author: {
+      name: {
+        label: string;
+      };
+      uri: {
+        label: string;
+      };
+    };
+    entry: {
+      "im:name": {
+        label: string;
+      };
+      "im:image": {
+        label: string;
+        attributes: {
+          height: number;
+        };
+      }[];
+      summary: {
+        label: string;
+      };
+      "im:price": {
+        label: string;
+        attributes: {
+          amount: string;
+          currency: "USD";
+        };
+      };
+      "im:contentType": {
+        attributes: {
+          term: "Podcast";
+          label: "Podcast";
+        };
+      };
+      rights: {
+        label: string;
+      };
+      title: {
+        label: string;
+      };
+      link: {
+        attributes: {
+          rel: "alternative";
+          type: "text/html";
+          href: string;
+        };
+      };
+      id: {
+        label: string;
+        attributes: {
+          "im:id": string;
+        };
+      };
+      "im:artist": {
+        label: string;
+        attributes: {
+          href: string;
+        };
+      };
+      category: {
+        attributes: {
+          "im:id": string;
+          term: "Music";
+          scheme: string;
+          label: "Music";
+        };
+      };
+      "im:releaseDate": {
+        label: string;
+        attributes: {
+          label: string;
+        };
+      };
+    }[];
   };
- };
- entry: {
-  "im:name": {
-   label: string;
-  };
-  "im:image": {
-   label: string;
-   attributes: {
-    height: number;
-   };
+}
+
+export interface IPodcastDetail {
+  resultCount: number;
+  results: {
+    trackId: number;
+    artisName: string;
+    trackName: string;
+    releaseDate: string;
+    trackTimeMillis: number;
+    description: string;
+    epidosdeUrl: string;
+    artworkUrl160: string;
+    episodeContentTipe: "audio";
+    shortDescription: string;
+    collectionName: string;
   }[];
-  summary: {
-   label: string;
-  };
-  "im:price": {
-   label: string;
-   attributes: {
-    amount: string;
-    currency: "USD";
-   };
-  };
-  "im:contentType": {
-   attributes: {
-    term: "Podcast";
-    label: "Podcast";
-   };
-  };
-  rights: {
-   label: string;
-  };
-  title: {
-   label: string;
-  };
-  link: {
-   attributes: {
-    rel: "alternative";
-    type: "text/html";
-    href: string;
-   };
-  };
-  id: {
-   label: string;
-   attributes: {
-    "im:id": string;
-   };
-  };
-  "im:artist": {
-   label: string;
-   attributes: {
-    href: string;
-   };
-  };
-  category: {
-   attributes: {
-    "im:id": string;
-    term: "Music";
-    scheme: string;
-    label: "Music";
-   };
-  };
-  "im:releaseDate": {
-   label: string;
-   attributes: {
-    label: string;
-   };
-  };
- }[];
 }
