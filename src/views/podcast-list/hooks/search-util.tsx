7fda1235fed4d981ebd89c09b0c325903f993577
wnopraf <rafael.prieto@chollometro.com>
2024-05-30 12:12:15 +0200

    improve search result algorithm

--- src/views/podcast-list/hooks/search-util.tsx
+++ src/views/podcast-list/hooks/search-util.tsx
@@ -4,18 +4,24 @@ import { PodCast } from "../../../types";
 export function useSearch(podcasts: PodCast[] | null) {
   const [search, setSearch] = useState<string>();
 
-  const filteredPodcasts = podcasts
-    ?.map((elm) => {
-      elm["im:name"].label = elm["im:name"].label.toLowerCase();
-      elm["im:artist"].label = elm["im:artist"].label.toLowerCase();
-      return elm;
-    })
-    .filter((elm) => {
-      return (
-        elm["im:name"].label.includes(search?.toLocaleLowerCase() as string) ||
-        elm["im:artist"].label.includes(search?.toLocaleLowerCase() as string)
-      );
-    });
+  let filteredPodcasts = podcasts?.map((elm) => {
+    elm["im:name"].label = elm["im:name"].label.toLowerCase();
+    elm["im:artist"].label = elm["im:artist"].label.toLowerCase();
+    return elm;
+  });
+  const filterdByAuthor = filteredPodcasts?.filter((elm) => {
+    return elm["im:artist"].label.includes(search?.toLowerCase() as string);
+  });
+  const filteredByName = filteredPodcasts?.filter((elm) => {
+    if (filterdByAuthor?.includes(elm)) {
+      return false;
+    }
+    return elm["im:name"].label.includes(search?.toLowerCase() as string);
+  });
+  filteredPodcasts = [
+    ...(filteredByName as PodCast[]),
+    ...(filterdByAuthor as PodCast[]),
+  ];
   return {
     setSearch,
     filteredPodcasts,
