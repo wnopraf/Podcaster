6aaa5c6590ad160e16999a2b138eeb39c343177c
wnopraf <rafael.prieto@chollometro.com>
2024-06-04 17:33:38 +0200

    add default error page

---
+++ src/ui/ErrorPage.tsx
@@ -0,0 +1,7 @@
+export function ErrorPage() {
+  return (
+    <div className="text-center min-h-[100vh] flex justify-center items-center">
+      <h1 className=" text-6xl font-semibold capitalize">page not avaliable</h1>
+    </div>
+  );
+}
