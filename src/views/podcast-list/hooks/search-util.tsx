3c2f2cf10dbe4f8e348eb1d64cbbc7b758851a52
wnopraf <rafael.prieto@chollometro.com>
2024-06-06 10:34:55 +0200

    fix no found search result

--- src/views/podcast-list/hooks/search-util.tsx
+++ src/views/podcast-list/hooks/search-util.tsx
@@ -1,26 +1,36 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
-export function useSearch(podcasts: Podcaster.PodCast[] | null) {
+export function useSearch(podcasts: Podcaster.PodCast[] | []) {
   const [search, setSearch] = useState<string>();
+  const filteredPodcasts = useRef(podcasts);
+  if (typeof search === "undefined" || search.length === 0) {
+    filteredPodcasts.current = podcasts;
+    return {
+      setSearch,
+      filteredPodcasts,
+    };
+  }
 
-  let filteredPodcasts = podcasts?.map((elm) => {
+  filteredPodcasts.current = podcasts?.map((elm) => {
     elm["im:name"].label = elm["im:name"].label.toLowerCase();
     elm["im:artist"].label = elm["im:artist"].label.toLowerCase();
     return elm;
   });
-  const filterdByAuthor = filteredPodcasts?.filter((elm) => {
-    return elm["im:artist"].label.includes(search?.toLowerCase() as string);
-  });
-  const filteredByName = filteredPodcasts?.filter((elm) => {
-    if (filterdByAuthor?.includes(elm)) {
-      return false;
-    }
-    return elm["im:name"].label.includes(search?.toLowerCase() as string);
-  });
-  filteredPodcasts = [
-    ...(filteredByName as Podcaster.PodCast[]),
-    ...(filterdByAuthor as Podcaster.PodCast[]),
-  ];
+  if (filteredPodcasts.current !== undefined) {
+    const filterdByAuthor = filteredPodcasts.current.filter((elm) => {
+      return elm["im:artist"].label.includes(search?.toLowerCase() as string);
+    });
+    const filteredByName = filteredPodcasts?.current.filter((elm) => {
+      if (filterdByAuthor?.includes(elm)) {
+        return false;
+      }
+      return elm["im:name"].label.includes(search?.toLowerCase() as string);
+    });
+    filteredPodcasts.current = [
+      ...(filteredByName as Podcaster.PodCast[]),
+      ...(filterdByAuthor as Podcaster.PodCast[]),
+    ];
+  }
   return {
     setSearch,
     filteredPodcasts,
--- src/views/podcast-list/index.tsx
+++ src/views/podcast-list/index.tsx
@@ -14,17 +14,16 @@ export function PodcastList() {
   const data = useLoaderData() as Podcaster.PodCast[];
   const { filteredPodcasts, setSearch } = useSearch(data);
 
-  const renderedPodcast = filteredPodcasts?.length ? filteredPodcasts : data;
   return (
     <div className="min-h-[75vh]">
       <PodcastSearch
         setSearch={setSearch}
-        filterResults={renderedPodcast?.length}
+        filterResults={filteredPodcasts?.current.length}
       />
       {/* auto-rows-[150px] to keep item height controled */}
       <div className=" mt-32 grid grid-cols-1 justify-items-center gap-x-12 gap-y-32 *:h-max sm:grid-cols-2 sm:justify-items-stretch lg:grid-cols-4 ">
         {/* <div className=" mt-[8rem] p-7 flex flex-wrap *:w-1/5 gap-x-[1rem] gap-y-[5rem] justify-evenly *:h-max"> */}
-        {renderedPodcast?.map((elm) => {
+        {filteredPodcasts?.current.map((elm) => {
           return (
             <PodcastItem
               title={elm["im:name"].label}
