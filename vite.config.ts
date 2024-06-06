e52655257a3f2c69732ffb76cb21e2738b94ca39
wnopraf <rafael.prieto@chollometro.com>
2024-06-06 14:06:24 +0200

    add build modes config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,24 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
 import tailwindcss from "tailwindcss";
 
 export default defineConfig({
- plugins: [react()],
- css: {
-  postcss: {
-   plugins: [tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
   },
- },
+  build: {
+    minify: process.env.NODE_ENV === "production" ? "esbuild" : false,
+    cssMinify: process.env.NODE_ENV === "production" ? "esbuild" : false,
+    sourcemap: process.env.NODE_ENV === "production" ? false : true,
+  },
+  resolve: {
+    alias: {
+      "@/": path.join(__dirname, "src/"),
+    },
+  },
 });
