f77947e5be58081b84b8aea15275e82edcda3afa
wnopraf <rafael.prieto@chollometro.com>
2024-06-07 11:56:36 +0200

    addd hearthBeat css anim

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,20 @@
 import type { Config } from "tailwindcss";
 
 export default {
- content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
- theme: {
-  extend: {},
- },
- plugins: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        hearthBeat: {
+          "0%": { opacity: ".75" },
+          "50%": { transform: "scale(1.25)", opacity: "1" },
+          "100%": { transform: "scale(.95)", opacity: ".9" },
+        },
+      },
+      animation: {
+        hearthBeat: "hearthBeat 1s ease-in-out infinite",
+      },
+    },
+  },
+  plugins: [],
 } satisfies Config;
