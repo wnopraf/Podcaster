eb5e7a74a60adc41d65666acde0a40795c820f3f
wnopraf <rafael.prieto@chollometro.com>
2024-06-07 19:31:38 +0200

    add cors service

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,5 +1,12 @@
+const ALLOW_ORIGINS_SERVICE = `https://api.allorigins.win/get?url=`;
 export const PODCAST_LIST_URL =
-  "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json";
-export const PODCAST_DETAIL_ROOT_URL = "https://itunes.apple.com/lookup?id=";
-export const PODCAST_DETAIL_QUERY_PARAMS_URL =
-  "&media=podcast&entity=podcastEpisode&limit=20";
+  ALLOW_ORIGINS_SERVICE +
+  encodeURIComponent(
+    "https://itunes.apple.com/us/rss/toppodcasts/limit=100/genre=1310/json"
+  );
+export const PODCAST_DETAIL_ROOT_URL =
+  ALLOW_ORIGINS_SERVICE +
+  encodeURIComponent("https://itunes.apple.com/lookup?id=");
+export const PODCAST_DETAIL_QUERY_PARAMS_URL = encodeURIComponent(
+  "&media=podcast&entity=podcastEpisode&limit=20"
+);
