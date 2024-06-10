29b5e347fe205d08044942eeed847440510a4d58
wnopraf <rafael.prieto@chollometro.com>
2024-06-10 18:31:05 +0200

    fix lint errors

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -2,13 +2,13 @@ export function SkeletonUi() {
   const skeletonData = new Array(12).fill(null);
   return (
     <>
-      <div className=" mt-4 flex flex-col items-center justify-center sm:flex-row sm:justify-between animate-pulse">
-        <span className=" mb-4 w-[30px] flex gap-y-1 justify-around sm:mb-0 sm:ml-auto sm:mr-2 p-1  bg-slate-300 text-white font-semibold rounded-sm">
-          <span className=" w-[2px] h-[10px] bg-white"></span>
-          <span className=" w-[2px] h-[10px] bg-white"></span>
-          <span className=" w-[2px] h-[10px] bg-white"></span>
+      <div className=" mt-4 flex animate-pulse flex-col items-center justify-center sm:flex-row sm:justify-between">
+        <span className=" mb-4 flex w-[30px] justify-around gap-y-1 rounded-sm bg-slate-300 p-1 font-semibold  text-white sm:mb-0 sm:ml-auto sm:mr-2">
+          <span className=" h-[10px] w-[2px] bg-white"></span>
+          <span className=" h-[10px] w-[2px] bg-white"></span>
+          <span className=" h-[10px] w-[2px] bg-white"></span>
         </span>
-        <input className="border border-2 rounded-sm" type="text" />
+        <input className=" rounded-sm border-2" type="text" />
       </div>
       <div className=" mt-32 grid grid-cols-1  gap-x-12 gap-y-32  sm:grid-cols-2 sm:justify-items-stretch lg:grid-cols-4 ">
         {skeletonData.map((elm, index) => {
@@ -21,11 +21,11 @@ export function SkeletonUi() {
 
 const SkeletonItem = () => {
   return (
-    <div className=" h-[77px] relative flex flex-col justify-center items-center p-3 shadow-md shadow-gray-400 animate-pulse">
-      <div className=" h-10 w-10  rounded-full bg-slate-300 w-20 h-20 absolute -top-1/2" />
+    <div className=" relative flex h-[77px] animate-pulse flex-col items-center justify-center p-3 shadow-md shadow-gray-400">
+      <div className="  absolute -top-1/2 size-20 rounded-full bg-slate-300" />
 
-      <h2 className=" mt-9 w-1/3 h-3 rounded-md  bg-slate-300"></h2>
-      <span className="mt-1 w-1/2 h-3 bg-slate-300 rounded-md"></span>
+      <h2 className=" mt-9 h-3 w-1/3 rounded-md  bg-slate-300"></h2>
+      <span className="mt-1 h-3 w-1/2 rounded-md bg-slate-300"></span>
     </div>
   );
 };
