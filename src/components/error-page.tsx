4a04dcac7f237af8e053088a99ef79b6baff95e7
wnopraf <rafael.prieto@chollometro.com>
2024-06-12 10:04:48 +0200

    change text color

--- src/components/error-page.tsx
+++ src/components/error-page.tsx
@@ -1,7 +1,9 @@
 export function ErrorPage() {
   return (
     <div className="flex min-h-screen items-center justify-center text-center">
-      <h1 className=" text-6xl font-semibold capitalize">page not avaliable</h1>
+      <h1 className=" text-6xl font-semibold capitalize text-gray-400">
+        page not avaliable
+      </h1>
     </div>
   );
 }
